ab67aae29cec88f02c1469c6ee7aeee73a0076ee
SupaxInc <paulj_gonzales@hotmail.com>
2025-03-02 00:01:12 -0500

    adding process image function

--- processing.py
+++ processing.py
@@ -3,8 +3,58 @@ import numpy as np
 from PIL import Image
 import torch
 
-IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
+IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5] # Values for RGB
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
+
+def resize(
+        image: Image,
+        size: Tuple[int, int],
+        resample: Image.Resampling = None,
+        reducing_gap: Optional[int] = None,
+) -> np.ndarray:
+    height, width = size
+    resized_image = image.resize(
+        (width, height), resample=resample, reducing_gap=reducing_gap
+    )
+
+    return resized_image
+
+def rescale(image: np.ndarray, scale: float, dtype: np.dtype = np.float32) -> np.ndarray:
+    rescaled_image = image * scale
+    rescaled_image = rescaled_image.astype(dtype)
+
+    return rescaled_image
+
+def normalize(image: np.ndarray, mean: Union[float, Iterable[float]], std: Union[float, Iterable[float]]) -> np.ndarray:
+    mean = np.array(mean, dtype=image.dtype)
+    std = np.array(std, dtype=image.dtype)
+    image = (image - mean) / std
+    return image
+
+def process_images(
+        images: List[Image.Image],
+        size: Dict[str, int] = None,
+        resample: Image.Resampling = None,
+        rescale_factor: float = None,
+        image_mean: Optional[Union[float, List[float]]] = None,
+        image_std: Optional[Union[float, list[float]]] = None,
+) -> List[np.ndarray]:
+    height, width = size[0], size[1]
+
+    # Resize the images based on the resample
+    images = [
+        resize(image=image, size=(height, width), resample=resample) for image in images
+    ]
+    # Convert each image to a numpy array
+    images = [np.array(image) for image in images]
+    # Rescale the pixel values to be in the range [0, 1]
+    images = [rescale(image, scale=rescale_factor) for image in images]
+    # Normalize the images to have mean 0 and standard deviation 1
+    images = [normalize(image, mean=image_mean, std=image_std) for image in images]
+    # Move the channel dimension to the first dimension. The model expects format [Channel, Height, Width]
+    images = [image.transpose(2, 0, 1) for image in images]
+
+    return images
 
 class PaliGemmaProcessor:
     # The tokenizer will only insert tokens for the text but we need a way to separate the image tokens
