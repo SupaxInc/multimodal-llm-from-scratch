d92b010fe6001b99086e72da5dbcb5e172beccbd
SupaxInc <paulj_gonzales@hotmail.com>
2025-03-23 22:42:32 -0400

    implemented transformer layers for gemma model

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -133,6 +133,59 @@ class GemmaRMSNorm(nn.Module):
         output = output * (1.0 + self.weight.float())
         return output.type_as(x)
 
+# * Transformer layers for Gemma model consisting of attention, FFN, and RMS normalization *
+class GemmaDecoderLayer(nn.Module):
+    def __init__(self, config: GemmaConfig, layer_idx: int):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+
+        self.self_attn = GemmaAttention(config=config, layer_idx=layer_idx)
+
+        self.mlp = GemmaMLP(config)
+        self.input_layernorm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(
+        self, 
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        kv_cache: Optional[KVCache] = None,
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+        # Grab reference of initial input for skip connection
+        residual = hidden_states
+        
+        # Apply initial layer normalization to input
+        # [B, seq_len, hidden_size]
+        hidden_states = self.input_layernorm(hidden_states)
+        # Apply the output of layer norm to self attention to mask the positional encodings
+        # [B, seq_len, hidden_size]
+        hidden_states, _, = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            kv_cache=kv_cache,
+        )
+        # Sum up the outputs of self attention with the skip connection
+        # [B, seq_len, hidden_size]
+        hidden_states = residual + hidden_states
+
+        # Grab reference of outputs for skip connection
+        # [B, seq_len, hidden_size]
+        residual = hidden_states
+
+        # Apply last layer normalization to the outputs
+        # [B, seq_len, hidden_size]
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        # Pass the outputs thru the feed forward network
+        # [B, seq_len, hidden_size]
+        hidden_states = self.mlp(hidden_states)
+        # Sum up the outputs with the skip connection
+        # [B, seq_len, hidden_size]
+        hidden_states = residual + hidden_states
+
+        return hidden_states
+    
 # * Gemma Model (language model), embedding layer + list of transformer layers*
 class GemmaModel(nn.Module):
     """Core Gemma transformer model that processes token embeddings through transformer layers.
